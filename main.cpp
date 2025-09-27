353434dfb5939801ef40f9165983e03c82f4cd40
Koraline's Dev House <51805645+koraldevhouse@users.noreply.github.com>
2025-09-27 00:29:14 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,34 +2,33 @@
 #include <cmath>
 #include <iomanip>
 
-
 int main(int argc, char** argv) {
 	
-	double w1, w2, m1, m2, m3;
+	double w1=0, w2=0, m1=0, m2=0, m3=0;
 	
-	std::cout << "1 semana:  ";
+	std::cout << "1 week:   ";
 	std::cin >> w1;
 	w1 = w1/10000;
 	
-	std::cout << "\n2 semanas: ";
+	std::cout << "2 weeks:  ";
 	std::cin >> w2;
 	w2 = w2/10000;
 	
 	if(w2!=0)
 	{
-		std::cout << "\n1 mes:   ";
+		std::cout << "1 month:  ";
 		std::cin >> m1;
 		m1 = m1/10000;
 		
 		if(m1!=0)
 		{
-			std::cout << "\n2 meses: ";
+			std::cout << "2 months: ";
 			std::cin >> m2;
 			m2 = m2/10000;
 			
 			if(m2!=0)
 			{
-				std::cout << "\n3 meses: ";
+				std::cout << "3 months: ";
 				std::cin >> m3;
 				m3 = m3/10000;
 			}
@@ -42,12 +41,13 @@ int main(int argc, char** argv) {
 	m2 = std::pow(1+m2, 3);
 	m3 = std::pow(1+m3, 2);
 	
-	std::cout << std::setprecision(15) << "\n\n\nApós 6 meses, os juros compostos são:\n\n" 
-				<< "\n1 semana:  " << w1
-				<< "\n2 semanas: " << w2
-				<< "\n1 mes:     " << m1
-				<< "\n2 meses:   " << m2
-				<< "\n3 meses:   " << m3 ;
+	std::cout << std::setprecision(15) << "\n\n\nAfter 180 days*, the compount interests are:\n\n" 
+				<< "\n1 week:   " << w1
+				<< "\n2 weeks:  " << w2
+				<< "\n1 month:  " << m1
+				<< "\n2 months: " << m2
+				<< "\n3 months: " << m3
+				<< "\n\n\n*1 and 2 weeks actually take 182 days :)";
 	
 	return 0;
 }
